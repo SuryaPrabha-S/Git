27b6eaee72456d345ab343cfc8f5850e2080a5a0
SuryaPrabha-S <prabhasuryas97@gmail.com>
2023-04-14 12:45:31 +0530

    3 methods

---
+++ src/test/java/org/test/GitClass.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class GitClass {
+private void india() {
+	System.out.println("AsianCountry");
+}
+private void british() {
+	System.out.println("Europe");
+}
+private void america() {
+	System.out.println("USA");
+
+}
+}
